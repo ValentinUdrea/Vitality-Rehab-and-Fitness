369031e369470a19ec97322b2e3a4dde1ca171a8
valentin <valentin.udrea1@yahoo.com>
2024-05-16 19:07:05 +0300

    Created all categories api

--- src/main/java/com/codeWithProjects/Vitality/controller/admin/AdminCategoryController.java
+++ src/main/java/com/codeWithProjects/Vitality/controller/admin/AdminCategoryController.java
@@ -4,12 +4,12 @@ import com.codeWithProjects.Vitality.dto.CategoryDto;
 import com.codeWithProjects.Vitality.entity.Category;
 import com.codeWithProjects.Vitality.services.admin.category.CategoryService;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.boot.jaxb.hbm.internal.CacheAccessTypeConverter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/admin")
@@ -22,5 +22,10 @@ public class AdminCategoryController {
         Category category = categoryService.createcategory(categoryDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(category);
 
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<Category>> getAllCategories(){
+        return ResponseEntity.ok(categoryService.getAllCategories());
     }
 }
--- src/main/java/com/codeWithProjects/Vitality/services/admin/category/CategoryService.java
+++ src/main/java/com/codeWithProjects/Vitality/services/admin/category/CategoryService.java
@@ -3,6 +3,9 @@ package com.codeWithProjects.Vitality.services.admin.category;
 import com.codeWithProjects.Vitality.dto.CategoryDto;
 import com.codeWithProjects.Vitality.entity.Category;
 
+import java.util.List;
+
 public interface CategoryService {
     Category createcategory(CategoryDto categoryDto);
+    List<Category> getAllCategories();
 }
